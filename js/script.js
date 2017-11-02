fc03f354a3ff29a8815cb3e67451820990726f7a
mikethepurple <mikethepurple@mikethepurples-MacBook-Pro-3.local>
2017-11-02 14:13:35 +0100

    Freaking horizontal scrolling

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
 $("#stbutton").on("click", function() {
-    $("#page1").css("display", "none");
-    $("#page2").css("display", "block");
-    console.log($('#industry option:selected').val());
-    if ($('#industry option:selected').val() == "ad") {
-        $("#title").append(" / ADVERTISING");
-    } else if (($('#industry option:selected').val() == "dev")) {
-        $("#title").append(" / DEV STUDIO");
-    }
+    // $("#page1").css("display", "none");
+    // $("#page2").css("display", "block");
+    // console.log($('#industry option:selected').val());
+    // if ($('#industry option:selected').val() == "ad") {
+    //     $("#title").append(" / ADVERTISING");
+    // } else if (($('#industry option:selected').val() == "dev")) {
+    //     $("#title").append(" / DEV STUDIO");
+    // }
+    window.location = "content";
+});
 
-    
-})
+$('#rightarrow').on("click", function() {
+    console.log("111");
+    event.preventDefault();
+    $('#guideitems').animate({
+      scrollRight: "+=300px"
+    }, "slow");
+ });
+
+
+// $(function() {
+// 	$('ul.nav a').bind('click',function(event){
+// 		var $anchor = $(this);
+// 		/*
+// 		if you want to use one of the easing effects:
+// 		$('html, body').stop().animate({
+// 			scrollLeft: $($anchor.attr('href')).offset().left
+// 		}, 1500,'easeInOutExpo');
+// 		 */
+// 		$('html, body').stop().animate({
+// 			scrollLeft: $($anchor.attr('href')).offset().left
+// 		}, 1000);
+// 		event.preventDefault();
+// 	});
+// });
