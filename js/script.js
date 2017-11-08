65913b1b0489898f1220c94af6c131eeea3a79b4
mikethepurple <mikethepurple@mikethepurples-MacBook-Pro-3.local>
2017-11-08 15:11:15 +0100

    ILYA PRIVET

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 $("#stbutton").on("click", function() {
     $("#page1").css("display", "none");
     $("#page2").css("display", "block");
@@ -10,13 +12,52 @@ $("#stbutton").on("click", function() {
     window.location = "content";
 });
 
-$('#rightarrow').on("click", function() {
-    console.log("111");
-    event.preventDefault();
-    $('#guideitems').animate({
-      scrollRight: "+=300px"
-    }, "slow");
- });
+
+
+
+var title = document.getElementById('tooltitle');
+var desc = document.getElementById('tooltext');
+var time = document.getElementById('time');
+var reqs = document.getElementById("tools");
+var people = document.getElementById("people");
+var sections = document.getElementById("sects");
+sections.innerHTML += "<div> <p class = \"title\">"+" contents.tools[i].sections[m].title" +"</p> <p class= \"text\">" + "contents.tools[i].sections[m].text" + "</p></div>";
+
+$.getJSON('/content/content.json', function(contents) {
+    $.each(contents.tools, function(i, items){
+        if(contents.tools[i].role == "designer") {
+            title.innerHTML = contents.tools[i].title;
+            desc.innerHTML = contents.tools[i].description;
+            time.innerHTML = contents.tools[i].time;
+            people.innerHTML = contents.tools[i].people;
+
+
+            $.each(contents.tools[i].tools, function(j, items) {
+                console.log(contents.tools[i].tools[j]);
+                reqs.innerHTML += (contents.tools[i].tools[j]+ "</br> ");
+            });
+            
+            
+            $.each(contents.tools[i].sections, function(m, items) {
+                console.log(contents.tools[i].sections[m].title);
+
+                // console.log(div.getElementById("title"));
+                console.log("i'm DIV")
+                sections.innerHTML += "<div> <p class = \"title\">"+ contents.tools[i].sections[m].title +"</p> <p class= \"text\">" + contents.tools[i].sections[m].text + "</p></div>";
+            });
+
+        }
+    });
+});
+
+
+// $('#rightarrow').on("click", function() {
+//     console.log("111");
+//     event.preventDefault();
+//     $('#guideitems').animate({
+//       scrollRight: "+=300px"
+//     }, "slow");
+//  });
 
  console.log("poop");
 
@@ -26,27 +67,6 @@ $('#rightarrow').on("click", function() {
   });
 
 
-var title = document.getElementById('tooltitle');
-var desc = document.getElementById('tooltext');
-var time = document.getElementById('time');
-var reqs = document.getElementById("tools");
-var people = document.getElementById("people");
-
-$.getJSON('/content/content.json', function(contents) {
-    $.each(contents.tools, function(i, items){
-        if(contents.tools[i].role == "designer") {
-            title.innerHTML = contents.tools[i].title;
-            desc.innerHTML = contents.tools[i].description;
-            time.innerHTML = contents.tools[i].time;
-            $.each(contents.tools[i].tools, function(j, items) {
-                console.log(contents.tools[i].tools[j]);
-                reqs.innerHTML += (contents.tools[i].tools[j]+ "</br> ");
-            });
-            people.innerHTML = contents.tools[i].people;
-
-        }
-    });
-});
 
 
 // $(function() {
